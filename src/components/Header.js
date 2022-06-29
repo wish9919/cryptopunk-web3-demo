94e75565928cdd4221d857bba315f4c192ce54f8
wish9919 <vishvantha321@gmail.com>
2022-06-30 01:11:04 +0530

    fix(styles): change header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,17 @@ import styles from "./Header.module.scss";
 const Header = () => {
   return (
     <header className={styles.Header}>
-      <div className={styles.LogoContainer}>
-        <img
-          src={"assets/header/cryptopunk-logo.png"}
-          className={styles.Logo}
-          alt="Cryptopunk logo"
-        />
+      <div className={styles.HeaderContainer}>
+        <div className={styles.LogoContainer}>
+          <img
+            src={"assets/header/cryptopunk-logo.png"}
+            className={styles.Logo}
+            alt="Cryptopunk logo"
+          />
+        </div>
+        <div className={styles.SearchContainer}>Search</div>
+        <div className={styles.NavContainer}>Menu</div>
       </div>
-      <div className={styles.SearchContainer}>Search</div>
-      <div className={styles.NavContainer}>Menu</div>
     </header>
   );
 };
